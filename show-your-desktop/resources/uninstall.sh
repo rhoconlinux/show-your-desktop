64d8de484c17ab70a19a166cda492e809e014c7c
rhoconlinux <rhoconlinux@gmail.com>
2014-05-10 15:18:57 +0200

    / ‘show-your-desktop/resources/uninstall.sh’

--- show-your-desktop/resources/uninstall.sh
+++ show-your-desktop/resources/uninstall.sh
@@ -7,5 +7,5 @@ sudo rm -f /usr/bin/shoot
 sudo rm -f /usr/bin/imgur
 sudo rm -f /usr/bin/show-your-desktop
 sudo rm -f /usr/share/applications/show-your-desktop.desktop
-rm -f ~/.config/plank/dock1/launchers/resources/show-your-desktop.dockitem
+rm -f ~/.config/plank/dock1/launchers/show-your-desktop.dockitem
 rm -Rfv ~/.show-your-desktop-installer
