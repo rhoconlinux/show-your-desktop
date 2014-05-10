0b27ae00510d97a5949af1377289d0196ce5d0ee
rhoconlinux <rhoconlinux@gmail.com>
2014-05-10 14:01:08 +0200

    / ‘install.sh’

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #cd ~ && rm -R  ~/.show-your-desktop-install ; sudo rm /usr/bin/imgur ; sudo rm /usr/bin/shoot ; sudo rm /usr/bin/snap-share ; sudo rm /usr/share/applications/snap-share.desktop ; rm ~/.icons/snap-share.svg ;
 
-cd show-your-desktop-master/show-your-desktop/ && sudo cp -a snap-share.svg /usr/share/icons/ && sudo chmod +x snap-share.desktop && sudo cp -a snap-share.desktop /usr/share/applications && chmod 755 shoot && sudo cp -a shoot /usr/bin && chmod 755 imgur && sudo cp -a imgur /usr/bin && sudo ln -s /usr/bin/shoot /usr/bin/snap-share && sudo cp -a snap-share.svg /usr/share/icons/hicolor/scalable
+cd show-your-desktop/
+#icon
+sudo cp -a resources/show-your-desktop.svg /usr/share/icons/ 
+#desktop-file
+
+&& sudo chmod +x snap-share.desktop && sudo cp -a snap-share.desktop /usr/share/applications && chmod 755 shoot && sudo cp -a shoot /usr/bin && chmod 755 imgur && sudo cp -a imgur /usr/bin && sudo ln -s /usr/bin/shoot /usr/bin/snap-share && sudo cp -a snap-share.svg /usr/share/icons/hicolor/scalable
