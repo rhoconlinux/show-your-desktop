9eeb5b6c632ed439f414d77990934065e18d529a
rhoconlinux <rhoconlinux@gmail.com>
2014-05-10 15:22:03 +0200

    / ‘show-your-desktop/resources/uninstall.sh’

--- show-your-desktop/resources/uninstall.sh
+++ show-your-desktop/resources/uninstall.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #the uninstaller
 cd
-sudo rm -f /usr/share/icons/resources/show-your-desktop.svg
-sudo rm -f /usr/share/icons/hicolor/scalable/show-your-desktop.svg
-sudo rm -f /usr/bin/shoot
-sudo rm -f /usr/bin/imgur
-sudo rm -f /usr/bin/show-your-desktop
-sudo rm -f /usr/share/applications/show-your-desktop.desktop
-rm -f ~/.config/plank/dock1/launchers/show-your-desktop.dockitem
-rm -Rfv ~/.show-your-desktop-installer
+sudo rm -f /usr/share/icons/resources/show-your-desktop.svg ;
+sudo rm -f /usr/share/icons/hicolor/scalable/show-your-desktop.svg ;
+sudo rm -f /usr/share/icons/hicolor/scalable/show-your-desktop.svg ;
+sudo rm -f /usr/bin/shoot ;
+sudo rm -f /usr/bin/imgur ;
+sudo rm -f /usr/bin/show-your-desktop ;
+sudo rm -f /usr/share/applications/show-your-desktop.desktop ;
+rm -f ~/.config/plank/dock1/launchers/show-your-desktop.dockitem ;
+rm -Rfv ~/.show-your-desktop-installer ;
