0a0fdf97236754a771787d7eef60aa4a88d3740e
rhoconlinux <rhoconlinux@gmail.com>
2014-05-10 15:17:03 +0200

    / ‘show-your-desktop/resources/uninstall.sh’

--- show-your-desktop/resources/uninstall.sh
+++ show-your-desktop/resources/uninstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #the uninstaller
+cd
 sudo rm -f /usr/share/icons/resources/show-your-desktop.svg
 sudo rm -f /usr/share/icons/hicolor/scalable/show-your-desktop.svg
 sudo rm -f /usr/bin/shoot
@@ -9,4 +10,3 @@ sudo rm -f /usr/share/applications/show-your-desktop.desktop
 rm -f ~/.config/plank/dock1/launchers/resources/show-your-desktop.dockitem
 rm -Rfv ~/.show-your-desktop-installer
 clear
-cd
