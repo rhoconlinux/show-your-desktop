41977a8b0af83cf7c9fde31b34721aef5c37a19b
rhoconlinux <rhoconlinux@gmail.com>
2014-05-11 12:10:47 +0200

    + ‘show-your-desktop/resources/install-gists.sh’

---
+++ show-your-desktop/resources/install-gists.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#the uninstaller
+cd
+sudo rm -f /usr/share/icons/resources/show-your-desktop.svg ;
+sudo rm -f /usr/share/icons/hicolor/scalable/show-your-desktop.svg ;
+sudo rm -f /usr/share/icons/hicolor/scalable/show-your-desktop.svg ;
+sudo rm -f /usr/share/icons/hicolor/scalable/apps/show-your-desktop.svg ;
+sudo rm -f /usr/bin/shoot ;
+sudo rm -f /usr/bin/imgur ;
+sudo rm -f /usr/bin/show-your-desktop ;
+sudo rm -f /usr/share/applications/show-your-desktop.desktop ;
+rm -f ~/.config/plank/dock1/launchers/show-your-desktop.dockitem ;
+rm -Rfv ~/.show-your-desktop-installer ;
