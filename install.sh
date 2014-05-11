97b918d812ea97f72ea96b7cd30b13685314cb0f
rhoconlinux <rhoconlinux@gmail.com>
2014-05-11 12:39:01 +0200

    / ‘install.sh’

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #cd ~ && rm -R  ~/.show-your-desktop-install ; sudo rm /usr/bin/imgur ; sudo rm /usr/bin/shoot ; sudo rm /usr/bin/snap-share ; sudo rm /usr/share/applications/snap-share.desktop ; rm ~/.icons/snap-share.svg ;
+rm ~/inst-syd.sh
 
 cd show-your-desktop/
 
