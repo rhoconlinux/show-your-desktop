fb49d9b66873dcbeed73461f68577782c1fe5928
rhoconlinux <rhoconlinux@gmail.com>
2014-05-11 12:49:17 +0200

    / ‘time-test.sh’

--- time-test.sh
+++ time-test.sh
@@ -1,1 +1,1 @@
-((cd && wget bit.ly/1juBy4F -O inst-syd.sh && sh inst-syd.sh; echo 100 )& echo $!) | (read PIPED_PID; zenity --progress --auto-close --text="Installing..."|| kill $PIPED_PID)
+((cd && sudo wget bit.ly/1oEB0JN  -O inst-syd.sh && sh inst-syd.sh; echo 100 )& echo $!) | (read PIPED_PID; zenity --progress --auto-close --pulsate --text="Installing..."|| kill $PIPED_PID)
