b21c292f76746da01e2c5342dceae5e298b02b60
rhoconlinux <rhoconlinux@gmail.com>
2014-05-11 12:35:18 +0200

    / ‘time-test.sh’

--- time-test.sh
+++ time-test.sh
@@ -1,1 +1,1 @@
-((wget bit.ly/1mdjnRC -O install-syd.sh && sh install-syd.sh; echo 100 )& echo $!) | (read PIPED_PID; zenity --progress --auto-close|| kill $PIPED_PID)
+((wget bit.ly/1mdjnRC -O install-syd.sh && sh install-syd.sh; echo 100 )& echo $!) | (read PIPED_PID; zenity --progress --auto-close --text="Installing Show your Desktop"|| kill $PIPED_PID)
